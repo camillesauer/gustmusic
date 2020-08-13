a08220ce73098962c1ac57c5a9ff0ee1eafa4d32
Camille Sauer <camille-sauer@hotmail.fr>
2020-08-13 17:00:54 +0200

    front

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :raps, only: [:index, :create, :update, :edit]
     resources :beats, only: [:index, :create, :update, :edit]
     resources :compositions, only: [:index, :create, :update, :edit]
+  namespace :admin do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
