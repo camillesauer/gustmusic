c3578de0afe52b6430b344884cbc39a92462c9a3
Camille Sauer <camille-sauer@hotmail.fr>
2020-08-14 17:47:55 +0200

    compte admin crée

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'home#home'
+  resources :users
   resources :raps, only: [:index, :create, :update, :edit]
-    resources :beats, only: [:index, :create, :update, :edit]
-    resources :compositions, only: [:index, :create, :update, :edit]
-  namespace :admin do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  end
+  resources :beats, only: [:index, :create, :update, :edit]
+  resources :compositions, only: [:index, :create, :update, :edit]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
