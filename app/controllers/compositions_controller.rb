88ac269c2bf131a822685e9d8dc93b3e687b1f0b
Camille Sauer <camille-sauer@hotmail.fr>
2020-08-15 00:23:13 +0200

    page done

--- app/controllers/compositions_controller.rb
+++ app/controllers/compositions_controller.rb
@@ -1,17 +1,17 @@
 class CompositionsController < ApplicationController
-   before_action :authenticate_user!,except:[:index]
-    def index
+  before_action :authenticate_user!,except:[:index]
+  def index
    @compositions = Composition.all
   end
-
   def new
     @composition = Composition.new # needed to instantiate the form_for
+
   end
 
   def create
-    @composition = Composition.new(params[:composition])
+    @composition = Composition.new(composition_params)
     @composition.save
-    redirect_to compositions_path
+    redirect_to compositions_path(@composition)
     # Will raise ActiveModel::ForbiddenAttributesError
   end
 
@@ -27,11 +27,15 @@ class CompositionsController < ApplicationController
   end
 
   def destroy
-    @composition = Composition.new(params[:composition])
+    @composition = Composition.find(params[:id])
     @composition.destroy
-
-    # no need for app/views/restaurants/destroy.html.erb
     redirect_to compositions_path
+  end
+
+  private
+
+  def composition_params
+    params.require(:composition).permit(:url)
   end
 end
 
--- app/controllers/raps_controller.rb
+++ app/controllers/raps_controller.rb
@@ -3,34 +3,38 @@ class RapsController < ApplicationController
   def index
    @raps = Rap.all
   end
-
   def new
     @rap = Rap.new # needed to instantiate the form_for
+
   end
 
   def create
-    @rap = Rap.new(params[:rap])
+    @rap = Rap.new(rap_params)
     @rap.save
-    redirect_to raps_path
+    redirect_to raps_path(@rap)
     # Will raise ActiveModel::ForbiddenAttributesError
   end
 
   def edit
-     @rap = Rap.find(params[:id])
+    @rap = Rap.find(params[:id])
   end
 
   def update
-     @rap = Rap.find(params[:id])
-     @rap.update(rap_params)
+    @rap = Rap.find(params[:id])
+    @rap.update(rap_params)
     # no need for app/views/restaurants/update.html.erb
-    redirect_to raps_path(@beat)
+    redirect_to raps_path(@rap)
   end
 
   def destroy
-     @rap = Rap.new(params[:rap])
-     @rap.destroy
-
-    # no need for app/views/restaurants/destroy.html.erb
+    @rap = Rap.find(params[:id])
+    @rap.destroy
     redirect_to raps_path
+  end
+
+  private
+
+  def rap_params
+    params.require(:rap).permit(:url)
   end
 end
